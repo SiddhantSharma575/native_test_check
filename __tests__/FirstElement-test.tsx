0cebb7562d54e4e309445a22bba4f278d4a90bf8
Siddhant Sharma <siddhantsharma909@gmail.com>
2023-05-18 18:44:00 +0530

    Text Test Done

--- __tests__/FirstElement-test.tsx
+++ __tests__/FirstElement-test.tsx
@@ -6,13 +6,16 @@ import renderer from 'react-test-renderer'
 
 describe("First ELement Check",  () => {
     it("Text Content check", async () => {
-        render(<FirstElementTest />)
-        const textElement = await screen.getByTestId("sample") 
-        
+         const renderer =   render(<FirstElementTest />)
+        // const textElement = await screen.getByTestId("sample") 
+        // expect(textElement)
+        const text = renderer.getByTestId("sample")
+        console.log(screen.getByTestId("sample").props)
+        expect(text.props.children).toBe("Text Element Check")
     })
 })
 
 // test("First Text Element check", () => {
-//     const tree = renderer.create(<FirstElementTest />).getInstance()
-//     console.log(tree)
+    // const tree = renderer.create(<FirstElementTest />).getInstance()
+    // console.log(tree)
 // })
